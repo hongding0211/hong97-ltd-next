1c3e5ae702a613011dfe9e597c2f7578f0cc8920
hongding0211 <keith.dh@hotmail.com>
2025-05-26 00:31:23 +0800

    feat: minor adjust

--- packages/fe/components/blog/common/comment/comments.tsx
+++ packages/fe/components/blog/common/comment/comments.tsx
@@ -11,7 +11,7 @@ const Comment: React.FC<CommentsResponseDto['comments'][0]> = (props) => {
   const { content, user, name } = props
 
   return (
-    <div className="flex flex-col gap-2.5 text-neutral-600 dark:text-neutral-400">
+    <div className="flex flex-col gap-1.5 text-neutral-600 dark:text-neutral-400">
       <div className="flex items-center gap-1.5">
         <Avatar
           user={{
