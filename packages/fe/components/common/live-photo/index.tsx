49dfe73a2b9680f89ad19ff4f218128c797d732e
hongding0211 <keith.dh@hotmail.com>
2025-06-02 02:05:11 +0800

    feat: blog update

--- packages/fe/components/common/live-photo/index.tsx
+++ packages/fe/components/common/live-photo/index.tsx
@@ -1,5 +1,6 @@
 import { cn } from '@/lib/utils'
 import { runOnClient } from '@utils/run-on-client'
+// @ts-ignore
 import type * as LivePhotosKit from 'LivePhotosKit'
 import React, { useEffect, useId, useRef } from 'react'
 
