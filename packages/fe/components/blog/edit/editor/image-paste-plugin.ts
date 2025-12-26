5cd6559391e6e8750c78b9cdd6ed7000bf85aa27
hongding0211 <keith.dh@hotmail.com>
2025-12-27 02:27:59 +0800

    feat: minor adjust

--- packages/fe/components/blog/edit/editor/image-paste-plugin.ts
+++ packages/fe/components/blog/edit/editor/image-paste-plugin.ts
@@ -22,22 +22,69 @@ export const createImagePastePlugin = (editor: Editor, nodeType: any) => {
 
         const { state, dispatch } = view
         const { tr, selection } = state
-        let currentPos = selection.to
+        const { from, to } = selection
 
-        imageFiles.forEach((file) => {
+        const $from = state.doc.resolve(from)
+        const currentNode = $from.parent
+
+        const isEmptyLine = currentNode.textContent.trim() === ''
+
+        let insertPos: number
+
+        if (isEmptyLine) {
+          const $to = state.doc.resolve(to)
+          const nodeStart = $from.before()
+          const nodeEnd = $to.after()
+
           const attrs: ReactMdxNodeAttrs = {
             name: 'img',
             props: JSON.stringify({ urls: '', caption: '' }),
           }
-          const node = nodeType.create(attrs)
-          tr.insert(currentPos, node)
+          const firstNode = nodeType.create(attrs)
+          tr.replaceRangeWith(nodeStart, nodeEnd, firstNode)
+          insertPos = nodeStart
 
+          const finalPos = insertPos
           setTimeout(() => {
-            handleImagePaste(editor, file, currentPos)
+            handleImagePaste(editor, imageFiles[0], finalPos)
           }, 0)
 
-          currentPos += node.nodeSize
-        })
+          insertPos += firstNode.nodeSize
+
+          for (let i = 1; i < imageFiles.length; i++) {
+            const attrs: ReactMdxNodeAttrs = {
+              name: 'img',
+              props: JSON.stringify({ urls: '', caption: '' }),
+            }
+            const node = nodeType.create(attrs)
+            tr.insert(insertPos, node)
+
+            const finalPos = insertPos
+            setTimeout(() => {
+              handleImagePaste(editor, imageFiles[i], finalPos)
+            }, 0)
+
+            insertPos += node.nodeSize
+          }
+        } else {
+          insertPos = $from.after()
+
+          imageFiles.forEach((file) => {
+            const attrs: ReactMdxNodeAttrs = {
+              name: 'img',
+              props: JSON.stringify({ urls: '', caption: '' }),
+            }
+            const node = nodeType.create(attrs)
+            tr.insert(insertPos, node)
+
+            const finalPos = insertPos
+            setTimeout(() => {
+              handleImagePaste(editor, file, finalPos)
+            }, 0)
+
+            insertPos += node.nodeSize
+          })
+        }
 
         dispatch(tr)
         return true
